c11e8feb53b5e91c74963653367d51ad1ecbc51a
Rachael <86849043+rachael-p@users.noreply.github.com>
2023-10-18 22:08:32 -0400

    Build-related changes and prettier/type-check edits

--- src/model.ts
+++ src/model.ts
@@ -1,16 +1,28 @@
 type NoteType = "natural" | "flat" | "sharp";
-type NotePitch = "C" | "D" | "E" | "F" | "G" | "A" | "B"; 
+type NotePitch = "C" | "D" | "E" | "F" | "G" | "A" | "B";
 
 interface Note {
-    type: NoteType;
-    pitch: NotePitch;
+  type: NoteType;
+  pitch: NotePitch;
 }
 
-type PianoKey = "C" | "CSharp" | "D" | "DSharp" | "E" | "F" | "FSharp" | "G" | "GSharp" | "A" | "ASharp" | "B" 
+type PianoKey =
+  | "C"
+  | "CSharp"
+  | "D"
+  | "DSharp"
+  | "E"
+  | "F"
+  | "FSharp"
+  | "G"
+  | "GSharp"
+  | "A"
+  | "ASharp"
+  | "B";
 
 type KeyVal = {
-    note: Note;
-    soundFile: string;
-}
+  note: Note;
+  soundFile: string;
+};
 
 type PianoMap = Record<PianoKey, KeyVal>;
